60031450840d04b45008fc9554943a324e5502f0
Alex Marquez <marquezworld@gmail.com>
2022-07-15 14:30:30 +0200

    Background foto en text toegevoegd

---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    var typed = new Typed(".typing-1",{
+        strings:[
+            "web Developer", "Allround DTP-er"
+        ],
+        typeSpeed: 90,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings:[
+            "web Developer", "Allround DTP-er"
+        ],
+        typeSpeed: 90,
+        backSpeed: 60,
+        loop: true
+    });
+    
+});
