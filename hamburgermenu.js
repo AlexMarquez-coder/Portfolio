11bb5188d6922b731143f63784644154af1230fa
Alex Marquez <marquezworld@gmail.com>
2022-08-22 14:26:37 +0200

    Navbar en hamburger menu gemaakt en responsive

---
+++ hamburgermenu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
